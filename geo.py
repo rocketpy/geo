9ef4b583ccaa8e6fa01d2490a9b9c5ef1a02aa5a
rocketpy <dimitr.ushakov@gmail.com>
2019-04-09 17:51:24 +0300

    updates

--- geo.py
+++ geo.py
@@ -8,3 +8,28 @@ g = geocoder.ip('me')
 print('Latitude, Longtitude =', g.latlng)  # input to google , see result at map
 print('ip Addres =', g.ip)
 print('Geolocation information\n', g.geojson)
+
+#  OR GEOCODER with Pandas and GEOPY
+
+# geocoding addresses with pandas and geopy
+# pip install geopy
+
+import geopy
+from geopy.geocoders import ArcGIS
+import pandas as pd
+
+nom=ArcGIS()
+
+# check some address
+n=nom.geocode("# input some address ")
+
+df=pd.read_csv("supermarkets.csv")
+df["Address"]=df["Address"]+", "+df["City"]+", "+df["State"]+", "+df["Country"]
+
+df["Coordinates"]=df["Address"].apply(nom.geocode)
+print(df)
+
+df.Coordinates[0].latitude
+
+df["Latitude"]=df["Coordinates"].apply(lambda x: x.latitude if x != None else None)
+df["Longtitude"]=df["Coordinates"].apply(lambda x: x.longtitude if x != None else None)
