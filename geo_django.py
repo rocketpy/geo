9e226378f7092a94c8c2b13639995c8346dc7b2e
rocketpy <dimitr.ushakov@gmail.com>
2019-12-17 20:20:03 +0200

    modified

---
+++ geo_django.py
@@ -0,0 +1,6 @@
+# installation : https://docs.djangoproject.com/en/3.0/ref/contrib/gis/install/
+
+# tutorials for creating a project :
+# https://www.techiediaries.com/django-gis-geodjango/
+# https://realpython.com/location-based-app-with-geodjango-tutorial/
+# https://docs.djangoproject.com/en/3.0/ref/contrib/gis/
