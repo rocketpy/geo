96f215fe6fdfaec23737968256a3934c9265668d
rocketpy <dimitr.ushakov@gmail.com>
2020-01-03 22:00:11 +0200

    modified

--- geoplotlib.py
+++ geoplotlib.py
@@ -1,1 +1,12 @@
+#  Python Toolbox for Geographic Visualizations !!!
 
+#  pip install geoplotlib
+
+# User  Guide  - https://github.com/andrea-cuttone/geoplotlib/wiki/User-Guide
+
+# simple example
+import geoplotlib
+
+data = geoplotlib.utils.read_csv('file_name.csv')
+geoplotlib.dot(data)
+geoplotlib.show()
