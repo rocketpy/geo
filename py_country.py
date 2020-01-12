d52446a13257d53f9a974c461ed2acfb4e3eea99
rocketpy <dimitr.ushakov@gmail.com>
2020-01-12 18:49:58 +0200

    modified

--- py_country.py
+++ py_country.py
@@ -1,1 +1,13 @@
+#  PYCOUNTRY is :  ISO country, subdivision, language, currency and script definitions and their translations
 
+#  all INFO taked from - https://pypi.org/project/pycountry/
+#  pip install pycountry
+
+#  simple example 
+
+import pycountry
+
+
+print(len(pycountry.countries))
+
+print(list(pycountry.countries)[0])
