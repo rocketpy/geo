2aa7871bc88f8e1fda9d54aa649af288720a7563
rocketpy <dimitr.ushakov@gmail.com>
2020-01-18 22:47:40 +0200

    modified

--- geopy_examples.py
+++ geopy_examples.py
@@ -1,1 +1,10 @@
+#  calculate the distance between 2 city's
+from geopy import distance
+
+
+first_city = ("51.5074° N, 0.1278° W")
+second_city = ("40.7128° N, 74.0060° W")
+print("Distance between first and second city's (in km):")
+print(distance.distance(first_city, second_city).km, " kms")
+
 
