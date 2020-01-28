a1435e3795a3af0088fba4f4f86b3d270d4a2c47
rocketpy <dimitr.ushakov@gmail.com>
2020-01-28 20:21:10 +0200

    added a new file

---
+++ yandex_geo.py
@@ -0,0 +1,13 @@
+#  library for getting address coordinates via Yandex geocoder
+
+#  OFFICIAL -  https://github.com/sivakov512/yandex-geocoder
+#  https://pypi.org/project/yandex-geocoder/
+
+#  pip install yandex-geocoder
+
+
+#  example
+from yandex_geocoder import Client
+
+Client.coordinates('address')
+# ('135.114326', '48.47839')
