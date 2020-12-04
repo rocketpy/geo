1982e859c6ba668d7f311261d401292aa0380fe4
rocketpy <dimitr.ushakov@gmail.com>
2020-12-04 21:29:22 +0200

    updated

--- pangeo.py
+++ pangeo.py
@@ -1,1 +1,7 @@
+#  PANGEO - A community platform for Big Data geoscience
 
+#  Official website: http://pangeo.io/
+#  GitHub: https://github.com/pangeo-data
+
+#  Pangeo Gallery: http://gallery.pangeo.io/
+#  Pangeo Notebooks:  https://github.com/pangeo-data/pangeo-example-notebooks
