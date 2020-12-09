8eba2639022e1ef2d3d7e12c3c83485b96fba8c9
rocketpy <dimitr.ushakov@gmail.com>
2020-12-09 15:25:01 +0200

    updated

--- pygal_maps.py
+++ pygal_maps.py
@@ -1,1 +1,19 @@
+#  Russian maps for Pygal
+
+# PyPi: https://pypi.org/project/pygal-maps-ru/
+
+# Requirements: Pygal - A python svg graph plotting library
+
+# pip install pygal
+# pip install pygal-maps-ru
+
+"""
+import pygal.maps.ru
+
+
+map = pygal.maps.ru.Regions()
+map.add('Data', {'KHM': 2, 'KYA': 5})
+map.render_to_file('test.svg')
+"""
+
 
