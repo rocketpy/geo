9d343b78c68308b324912b8b29a52b7b42b2be5f
rocketpy <dimitr.ushakov@gmail.com>
2021-01-07 22:13:34 +0200

    Update geoplot.py

--- geoplot.py
+++ geoplot.py
@@ -1,7 +1,19 @@
-#  geoplot is a geospatial data visualization library 
+#  High-level geospatial plotting for Python.
 
 # Github: https://github.com/ResidentMario/geoplot
 # Docs: https://residentmario.github.io/geoplot/installation.html
 
+#  Important !!!  Requires: Python >=3.6.0
 #  pip install geoplot
 
+%matplotlib inline
+
+import pandas as pd; pd.set_option('max_columns', 6)
+import geopandas as gpd
+import geoplot as gplt
+
+
+usa_cities = gpd.read_file(gplt.datasets.get_path('usa_cities'))
+usa_cities.head() 
+
+# more examples: https://residentmario.github.io/geoplot/quickstart/quickstart.html 
