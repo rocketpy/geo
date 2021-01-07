4ee825ab462a6d4660c0f73bbba671b3162d3ae6
rocketpy <dimitr.ushakov@gmail.com>
2021-01-07 22:09:26 +0200

    Update geoplot.py

--- geoplot.py
+++ geoplot.py
@@ -1,1 +1,7 @@
+#  geoplot is a geospatial data visualization library 
+
+# Github: https://github.com/ResidentMario/geoplot
+# Docs: https://residentmario.github.io/geoplot/installation.html
+
+#  pip install geoplot
 
