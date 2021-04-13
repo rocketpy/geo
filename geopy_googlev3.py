dcf71f485353e6c5c43656ef575fd341615ae736
rocketpy <dimitr.ushakov@gmail.com>
2021-04-13 22:56:25 +0300

    Update geopy_googlev3.py

--- geopy_googlev3.py
+++ geopy_googlev3.py
@@ -1,1 +1,10 @@
+from geopy import GoogleV3
+# Docs:  https://geopy.readthedocs.io/en/stable/
+# Get API Key: https://geopy.readthedocs.io/en/stable/#googlev3
 
+
+place = "Some place name or city"
+location = GoogleV3(api_key="HERE YOUR API KEY", domain="maps.google.ua").geocode(place)
+
+print(location.address)
+print(location.latitude, location.longitude)
