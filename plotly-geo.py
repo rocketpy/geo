8145bd99b0b770fba6675df7e80e9cce3655771f
rocketpy <dimitr.ushakov@gmail.com>
2021-05-19 22:20:41 +0300

    Update plotly-geo.py

--- plotly-geo.py
+++ plotly-geo.py
@@ -1,1 +1,9 @@
+# plotly-geo - geo shape files for use with plotly.py
 
+# PyPi: https://pypi.org/project/plotly-geo/
+# Github: https://github.com/plotly/plotly.py
+# Plotly: https://plotly.com/python/
+
+
+# Project description
+# Package containing the geo shape files used by plotly.py
