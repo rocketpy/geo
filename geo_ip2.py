e3dcf7255f22fa7d118837425725ded9c6f5e9e0
rocketpy <dimitr.ushakov@gmail.com>
2022-03-20 22:53:18 +0200

    Update geo_ip2.py

--- geo_ip2.py
+++ geo_ip2.py
@@ -3,6 +3,9 @@
 
 # https://pypi.org/project/geoip2/
 
+# MaxMind GeoIP2 API
+# https://www.maxmind.com/en/home
+
 # pip install geoip2
 
 
@@ -32,4 +35,30 @@ with geoip2.webservice.Client(42, 'license_key') as client:
 # response.traits.network
 
 # IPv4Network('203.0.113.0/32')
+
+
+#  Async Web Service Example
+
+import asyncio
+import geoip2.webservice
+
+
+async def main():
+    """
+    This creates an AsyncClient object that can be reused across
+    requests on the running event loop. If you are using multiple event
+    loops, you must ensure the object is not used on another loop.
+
+    Replace "42" with your account ID and "license_key" with your license
+    key. Set the "host" keyword argument to "geolite.info" to use the
+    GeoLite2 web service instead of GeoIP2 Precision.
+    """
+    async with geoip2.webservice.AsyncClient(42, 'license_key') as client:
+        """
+        Replace "city" with the method corresponding to the web service
+        that you are using, i.e., "country", "city", or "insights". Please
+        note that Insights is not supported by the GeoLite2 web service.
+        """
+        response = await client.city('203.0.113.0')
+        response.country.iso_code
 
