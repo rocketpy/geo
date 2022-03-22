dd87253316801baa3c3f1afa6c56571e078227f4
rocketpy <dimitr.ushakov@gmail.com>
2022-03-22 23:58:52 +0200

    Update geo_ip2.py

--- geo_ip2.py
+++ geo_ip2.py
@@ -90,3 +90,24 @@ with geoip2.database.Reader('/path/to/GeoLite2-City.mmdb') as reader:
     response = reader.city('203.0.113.0')
     response.country.iso_code
     response.country.name
+    response.country.names['zh-CN']
+u'美国'
+>>>
+>>>     response.subdivisions.most_specific.name
+'Minnesota'
+>>>     response.subdivisions.most_specific.iso_code
+'MN'
+>>>
+>>>     response.city.name
+'Minneapolis'
+>>>
+>>>     response.postal.code
+'55455'
+>>>
+>>>     response.location.latitude
+44.9733
+>>>     response.location.longitude
+-93.2323
+>>>
+>>>     response.traits.network
+IPv4Network('203.0.113.0/24')
