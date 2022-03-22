c726a5da222de21a56c6d66b2de41c32d0f1f2c0
rocketpy <dimitr.ushakov@gmail.com>
2022-03-23 00:05:59 +0200

    Update geo_ip2.py

--- geo_ip2.py
+++ geo_ip2.py
@@ -87,27 +87,18 @@ with geoip2.database.Reader('/path/to/GeoLite2-City.mmdb') as reader:
 
     # Replace "city" with the method corresponding to the database
     # that you are using, e.g., "country".
+    """
     response = reader.city('203.0.113.0')
     response.country.iso_code
     response.country.name
     response.country.names['zh-CN']
-u'美国'
->>>
->>>     response.subdivisions.most_specific.name
-'Minnesota'
->>>     response.subdivisions.most_specific.iso_code
-'MN'
->>>
->>>     response.city.name
-'Minneapolis'
->>>
->>>     response.postal.code
-'55455'
->>>
->>>     response.location.latitude
-44.9733
->>>     response.location.longitude
--93.2323
->>>
->>>     response.traits.network
-IPv4Network('203.0.113.0/24')
+    response.subdivisions.most_specific.name
+    response.subdivisions.most_specific.iso_code
+    response.city.name
+    response.postal.code
+    response.location.latitude
+    response.location.longitude
+    response.traits.network
+    """
+# IPv4Network('203.0.113.0/24')
+
