0a148b92a03b20a5e6e57f1ffdf6b5f0c41ab253
rocketpy <dimitr.ushakov@gmail.com>
2022-10-21 20:53:20 +0000

    Update fake_geospatial_images.py

--- fake_geospatial_images.py
+++ fake_geospatial_images.py
@@ -1,1 +1,14 @@
+# fake-geo-images - Fake geospatial images for unit tests
+
+# https://pypi.org/project/fake-geo-images/
+# https://github.com/up42/fake-geo-images
+
+# pip install fake-geo-images
+
+
+import numpy as np
+import rasterio as rio
+from pathlib import Path
+from my_image_processing import ndvi
+from fake_geo_images.fakegeoimages import FakeGeoImage
 
