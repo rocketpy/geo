8f85642392c2237bdf251127f31ac2002a31db33
rocketpy <dimitr.ushakov@gmail.com>
2022-10-22 20:57:13 +0000

    Update fake_geospatial_images.py

--- fake_geospatial_images.py
+++ fake_geospatial_images.py
@@ -12,3 +12,24 @@ from pathlib import Path
 from my_image_processing import ndvi
 from fake_geo_images.fakegeoimages import FakeGeoImage
 
+
+def test_ndvi():
+    """
+    A unit test if an NDVI method works in general
+    """
+    # Create 4-band image simulating RGBN as needed for NDVI
+    test_image, _ = FakeGeoImage(
+            300, 150, 4, "uint16", out_dir=Path("/tmp"), crs=4326, nodata=0, nodata_fill=3, cog = False
+        ).create(seed=42)
+
+    ndvi_image = ndvi(test_image)
+
+    with rio.open(str(ndvi_image)) as src:
+        ndvi_array = src.read()
+        # NDVI only has one band of same size as input bands
+        assert ndvi_array.shape == (1, 300, 150)
+        # NDVI has float values between -1 and 1
+        assert ndvi_array.dtype == np.float
+        assert ndvi_array.min >= -1
+        assert ndvi_array.max <= 1
+
