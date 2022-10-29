6eaa523a3945b8d1a5f4eb68e73254b27dc884d6
rocketpy <dimitr.ushakov@gmail.com>
2022-10-29 20:54:48 +0000

    Update ArcPy.py

--- ArcPy.py
+++ ArcPy.py
@@ -1,1 +1,6 @@
+# ArcPy – is a Python site package that enables successful and productive geographic data analysis,
+# data conversion, data management, and map automation in Python.
 
+# The default ArcPy package is part of the arcgispro-py3 Python distribution that ships with ArcGIS Pro and ArcGIS Server !!!
+
+# Install and set up - https://developers.arcgis.com/python/guide/install-older-versions/
