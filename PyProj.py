464464bb0944fc9d1e3e93456e0b780c3bdd0c5b
rocketpy <dimitr.ushakov@gmail.com>
2022-10-30 21:50:08 +0000

    Update PyProj.py

--- PyProj.py
+++ PyProj.py
@@ -1,1 +1,15 @@
+# Python interface to PROJ (cartographic projections and coordinate transformations library).
 
+# GitHub: https://github.com/pyproj4/pyproj
+
+# pip install pyproj
+# Install pyproj: https://pyproj4.github.io/pyproj/stable/installation.html#install-pyproj
+
+
+# Using CRS
+from pyproj import CRS
+
+crs = CRS.from_epsg(4326)
+crs = CRS.from_string("epsg:4326")
+crs = CRS.from_proj4("+proj=latlon")
+crs = CRS.from_user_input(4326)
