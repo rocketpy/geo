1a525ddeacd9333ab4e644d926d306b9369364b2
rocketpy <dimitr.ushakov@gmail.com>
2022-11-02 21:42:02 +0000

    Update PyProj.py

--- PyProj.py
+++ PyProj.py
@@ -28,18 +28,25 @@ print(crs.to_authority())
 
 crs = CRS.from_proj4("+proj=omerc +lat_0=-36 +lonc=147 +alpha=-54 +k=1 +x_0=0 +y_0=0 +gamma=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0")
 print(crs)
-
 print(crs.to_wkt(pretty=True))
-
 
 from pyproj.enums import WktVersion
 print(crs.to_wkt(WktVersion.WKT1_GDAL, pretty=True))
 
-
 from pprint import pprint
 pprint(crs.to_cf())
-
 
 # Extracting attributes from CRS
 crs = CRS("urn:ogc:def:crs,crs:EPSG::2393,crs:EPSG::5717")
 print(crs)
+
+print(crs.sub_crs_list)
+
+cop = crs.sub_crs_list[0].coordinate_operation
+print(cop.to_wkt(pretty=True))
+
+print(cop.method_code)
+# '9807'
+print(cop.method_name)
+# 'Transverse Mercator'
+print(cop.params)
