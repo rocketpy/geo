2fc74fa4b815ee2bf9a296e8e22b62f907e5a498
rocketpy <dimitr.ushakov@gmail.com>
2022-11-10 21:20:55 +0000

    Update PyProj.py

--- PyProj.py
+++ PyProj.py
@@ -146,3 +146,14 @@ line_string = LineString([Point(1, 2), Point(3, 4)]))
 geod = Geod(ellps="WGS84")
 total_length = geod.geometry_length(line_string)
 print(f"{total_length:.3f}")
+
+
+# Geodesic area
+from pyproj import Geod
+
+geod = Geod('+a=6378137 +f=0.0033528106647475126')
+lats = [-72.9, -71.9, -74.9, -74.3, -77.5, -77.4, -71.7, -65.9, -65.7,
+        -66.6, -66.9, -69.8, -70.0, -71.0, -77.3, -77.9, -74.7]
+
+lons = [-74, -102, -102, -131, -163, 163, 172, 140, 113,
+        88, 59, 25, -4, -14, -33, -46, -61]
