92c111316fd2195d558e640afb8f9aa6d647f10c
rocketpy <dimitr.ushakov@gmail.com>
2023-06-16 23:55:16 +0300

    Create PySatNASA.py

---
+++ PySatNASA.py
@@ -0,0 +1,9 @@
+# pysatNASA: pysat support for NASA Space Science instruments
+
+# https://github.com/pysat/pysatNASA
+
+# git clone https://github.com/pysat/pysatNASA.git
+
+# Change directories into the repository folder and run the setup.py file. For a local install use the "--user" flag after "install".
+# cd pysatNASA/
+# python setup.py install
