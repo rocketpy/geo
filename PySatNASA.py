0bb1bb7bbebf79c5fa1d6799d5fe5700e2c13416
rocketpy <dimitr.ushakov@gmail.com>
2023-06-17 23:49:20 +0300

    Update PySatNASA.py

--- PySatNASA.py
+++ PySatNASA.py
@@ -1,9 +1,16 @@
 # pysatNASA: pysat support for NASA Space Science instruments
 
 # https://github.com/pysat/pysatNASA
-
 # git clone https://github.com/pysat/pysatNASA.git
 
-# Change directories into the repository folder and run the setup.py file. For a local install use the "--user" flag after "install".
+# Change directories into the repository folder and run the setup.py file.
+# For a local install use the "--user" flag after "install".
 # cd pysatNASA/
 # python setup.py install
+
+
+# Using with pysat
+import pysat
+from pysatNASA.instruments import icon_ivm
+
+ivm = pysat.Instrument(inst_module=icon_ivm, inst_id='a')
