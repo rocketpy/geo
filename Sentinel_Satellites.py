bdd7abe05c7ed01fea50aa78935eb1a01b84b734
rocketpy <dimitr.ushakov@gmail.com>
2023-06-30 23:18:48 +0300

    Create Sentinel_Satellites.py

---
+++ Sentinel_Satellites.py
@@ -0,0 +1,4 @@
+# A Python library that allows to extract data from sentinel satellites, exploiting multi-threading and relying on Google Earth Engine APIs.
+
+# https://github.com/Amatofrancesco99/master-thesis/tree/main/Notebooks
+# https://pypi.org/project/sentinel-satellites/
