11eed2dbd56e802a12e0d247ada4cd84fa905382
rocketpy <dimitr.ushakov@gmail.com>
2023-07-01 23:55:40 +0300

    Update Sentinel_Satellites.py

--- Sentinel_Satellites.py
+++ Sentinel_Satellites.py
@@ -2,3 +2,13 @@
 
 # https://github.com/Amatofrancesco99/master-thesis/tree/main/Notebooks
 # https://pypi.org/project/sentinel-satellites/
+
+# pip install sentinel-satellites
+
+
+# First you have to initialize and authorize the Google Earth Engine APIs.
+import ee, sentinel_satellites
+
+
+ee.Authenticate()
+ee.Initialize()
