4ec7bc50a01d278953db96bc12da703bd59a4320
rocketpy <dimitr.ushakov@gmail.com>
2023-10-24 23:28:46 +0300

    Create Astropy.py

---
+++ Astropy.py
@@ -0,0 +1,6 @@
+# Astropy - package for Astronomy in Python and foster interoperability between Python astronomy packages.
+
+# https://github.com/astropy/astropy
+# http://astropy.org/
+# https://pypi.org/project/astropy/
+# https://docs.astropy.org/en/stable/
