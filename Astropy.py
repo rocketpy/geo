1b8d88f2784072d3e26f686fc12d19876992c360
rocketpy <dimitr.ushakov@gmail.com>
2023-10-25 21:52:25 +0300

    Update Astropy.py

--- Astropy.py
+++ Astropy.py
@@ -4,3 +4,15 @@
 # http://astropy.org/
 # https://pypi.org/project/astropy/
 # https://docs.astropy.org/en/stable/
+
+
+import astropy
+from astropy import subpackage
+from astropy.io import fits
+
+hdulist = fits.open('data.fits')
+
+
+from astropy import units as u
+from astropy import coordinates as coord
+coord.SkyCoord(ra=10.68458*u.deg, dec=41.26917*u.deg, frame='icrs')
