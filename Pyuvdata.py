1373debdb48cdfca7cc271273288e1b760210b5f
rocketpy <dimitr.ushakov@gmail.com>
2023-11-02 23:29:50 +0200

    Update Pyuvdata.py

--- Pyuvdata.py
+++ Pyuvdata.py
@@ -1,3 +1,26 @@
 # pyuvdata - defines a pythonic interface to interferometric data sets.
 
 # https://github.com/RadioAstronomySoftwareGroup/pyuvdata
+# Docs - https://pyuvdata.readthedocs.io/en/latest/
+
+# The latest stable version is available via conda (preferred: conda install -c conda-forge pyuvdata)
+# or by pip (pip install pyuvdata).
+
+"""
+The main goals are:
+To provide a high quality, well documented path to convert between data formats
+Support the direct use of datasets from python with minimal software
+Provide precise data definition via both human readable code and high quality online documentation
+"""
+
+"""
+Dependencies:
+astropy >= 5.0.4
+docstring_parser>=0.15
+h5py >= 3.1
+numpy >= 1.20
+pyerfa >= 2.0
+scipy >= 1.5
+setuptools_scm <7.0|>=7.0.3
+"""
+
