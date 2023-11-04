f4ce32ef7f3f312464b581c84ad92241b8f2caf8
rocketpy <dimitr.ushakov@gmail.com>
2023-11-04 23:55:15 +0200

    Update Stingray.py

--- Stingray.py
+++ Stingray.py
@@ -1,4 +1,12 @@
-Stingray - X-Ray Spectral Timing Made Easy
+# Stingray - X-Ray Spectral Timing Made Easy
 
 # https://github.com/StingraySoftware/stingray
+# Docs - https://docs.stingray.science/en/v1.1.2.2/
 
+# pip install stingray
+# pip install tox
+
+
+import stingray
+
+stingray.test()
