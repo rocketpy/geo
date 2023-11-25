8a77e8e1a719eb283efb79b6c46f3a42e266543b
rocketpy <dimitr.ushakov@gmail.com>
2023-11-25 23:19:51 +0200

    Create astronomical_data_analysis_visualization.py

---
+++ astronomical_data_analysis_visualization.py
@@ -0,0 +1,14 @@
+# jdaviz is a package of astronomical data analysis visualization tools based on the Jupyter platform.
+
+# https://github.com/spacetelescope/jdaviz
+
+# Installation
+"""
+conda create -n jdaviz-env python=3.9
+conda activate jdaviz-env
+
+pip install git+https://github.com/spacetelescope/jdaviz --upgrade
+
+pip install jdaviz --upgrade
+"""
+
