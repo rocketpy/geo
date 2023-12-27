9039be87558fec355f0dcea1d3185b8771702c6c
rocketpy <dimitr.ushakov@gmail.com>
2023-12-27 23:52:21 +0200

    Create Py_tides.py

---
+++ Py_tides.py
@@ -0,0 +1,20 @@
+# Pytides - Pytides is small Python package for the analysis and prediction of tides.
+
+# https://github.com/sam-cox/pytides/
+
+# Installation
+"""
+easy_install pytides
+or
+pip install pytides
+should do the trick.
+Mainly for my own reference (sanity), to get pytides and its dependencies all working in a Debian (mint) virtualenv:
+
+sudo apt-get install liblapack-dev libatlas-base-dev gfortran
+export LAPACK=/usr/lib/liblapack.so
+export ATLAS=/usr/lib/libatlas.so
+export BLAS=/usr/lib/libblas.so
+pip install numpy
+pip install scipy
+pip install pytides
+"""
